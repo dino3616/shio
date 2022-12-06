bc4d32b1cbd709a53cea68bf2b474ca8f3aaab40
shio <85730998+dino3616@users.noreply.github.com>
2022-12-06 02:08:37 +0000

    initial: 🎉 first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center">shio</h1>
+
+![favicon](./doc/favicon.png)
---
+++ packages/api/src/infra/prisma/generated/user/user-group-by/output.ts
@@ -0,0 +1,24 @@
+import { Field } from '@nestjs/graphql';
+import { ObjectType } from '@nestjs/graphql';
+import { UserCountAggregate } from '../user-count-aggregate/output';
+import { UserMinAggregate } from '../user-min-aggregate/output';
+import { UserMaxAggregate } from '../user-max-aggregate/output';
+
+@ObjectType()
+export class UserGroupBy {
+
+    @Field(() => String, {nullable:false})
+    id!: string;
+
+    @Field(() => String, {nullable:false})
+    name!: string;
+
+    @Field(() => UserCountAggregate, {nullable:true})
+    _count?: UserCountAggregate;
+
+    @Field(() => UserMinAggregate, {nullable:true})
+    _min?: UserMinAggregate;
+
+    @Field(() => UserMaxAggregate, {nullable:true})
+    _max?: UserMaxAggregate;
+}
