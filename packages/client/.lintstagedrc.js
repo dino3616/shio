3982d5cf718c2b683d324334d4b36fd4d50b8b42
shio <85730998+dino3616@users.noreply.github.com>
2022-12-09 01:57:45 +0000

    fix: 🐞 (lint-staged) 不要なコメントアウトを削除

--- packages/client/.lintstagedrc.js
+++ packages/client/.lintstagedrc.js
@@ -1,8 +1,3 @@
-// {
-//   "*.{js,ts,tsx}": "yarn lint:fix",
-//   "*.{js,ts,json}": "yarn fmt:fix",
-//   "*.{css,scss}": "yarn style:fix"
-// }
 module.exports = {
   '**/*.{js,ts,tsx,json}': (filenames) => `yarn lint:fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(' --file ')}`,
   '**/*.{js,ts,tsx,json}': (filenames) => `yarn fmt:fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(' --file ')}`,
