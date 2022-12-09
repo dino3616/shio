e2c9f1613ccf1f08f67bda7c3a84008394ed6b6e
shio <85730998+dino3616@users.noreply.github.com>
2022-12-09 17:34:29 +0000

    chore: 🔧 (client/lint-staged) css及びscssファイルへ実行するコマンドを修正

--- packages/client/.lintstagedrc.js
+++ packages/client/.lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '**/*.{js,ts,tsx,json}': (filenames) => `yarn lint:fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(' --file ')}`,
   '**/*.{js,ts,tsx,json}': (filenames) => `yarn fmt:fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(' --file ')}`,
-  '**/*.{css,scss}': (filenames) => `yarn style:fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(' --file ')}`,
+  '**/*.{css,scss}': 'yarn style:fix',
 };
