ca4276d69c386d0f1f953810950a9cd1051860a7
shio <85730998+dino3616@users.noreply.github.com>
2022-12-09 01:56:50 +0000

    fix: 🐞 (client) prettierを実行

---
+++ packages/api/.lintstagedrc.js
@@ -0,0 +1,8 @@
+// {
+//   "*.ts": "yarn lint:fix",
+//   "*.{js,ts,json}": "yarn fmt:fix"
+// }
+module.exports = {
+  '**/*.{js,ts,json}': (filenames) => `yarn lint:fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(' --file ')}`,
+  '**/*.{js,ts,json}': (filenames) => `yarn fmt:fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(' --file ')}`,
+};
---
+++ packages/client/.lintstagedrc.js
@@ -0,0 +1,10 @@
+// {
+//   "*.{js,ts,tsx}": "yarn lint:fix",
+//   "*.{js,ts,json}": "yarn fmt:fix",
+//   "*.{css,scss}": "yarn style:fix"
+// }
+module.exports = {
+  '**/*.{js,ts,tsx,json}': (filenames) => `yarn lint:fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(' --file ')}`,
+  '**/*.{js,ts,tsx,json}': (filenames) => `yarn fmt:fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(' --file ')}`,
+  '**/*.{css,scss}': (filenames) => `yarn style:fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(' --file ')}`,
+};
