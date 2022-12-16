9cddf28ac109e4bb47340ff3eb7a49abe0fe12aa
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-12-16 10:48:21 +0900

    fix: 🐞 自動生成されたファイルを.gitignoreに指定

--- packages/api/src/infra/prisma/generated/user/user-scalar-field/enum.ts
+++
@@ -1,9 +0,0 @@
-import { registerEnumType } from '@nestjs/graphql';
-
-export enum UserScalarFieldEnum {
-    id = "id",
-    name = "name"
-}
-
-
-registerEnumType(UserScalarFieldEnum, { name: 'UserScalarFieldEnum', description: undefined })
--- packages/api/src/infra/prisma/generated/user/user-unchecked-update/input.ts
+++
@@ -1,9 +0,0 @@
-import { Field } from '@nestjs/graphql';
-import { InputType } from '@nestjs/graphql';
-
-@InputType()
-export class UserUncheckedUpdateInput {
-
-    @Field(() => String, {nullable:true})
-    name?: string;
-}
