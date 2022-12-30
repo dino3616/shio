a83905a34ea0bbf0d118c697cdf0e0aecad5794e
shio <noir3616@gmail.com>
2022-12-30 09:45:45 +0000

    fix: 🐞 (footer) view portが小さい場合avatarは表示しないように修正

--- packages/client/src/module/root/ui/footer.layout.tsx
+++ packages/client/src/module/root/ui/footer.layout.tsx
@@ -6,9 +6,9 @@ export type FooterProps = Omit<ComponentPropsWithoutRef<'footer'>, 'children'>;
 
 export const Footer: FC<FooterProps> = ({ className, ...props }) => (
   <footer className={twMerge('flex flex-col items-center justify-center space-y-10 bg-accent py-10 text-primary-800', className)} {...props}>
-    <div className="flex items-center space-x-5">
+    <div className="flex items-center gap-5">
       <span className="text-5xl font-semibold">Shio.dev</span>
-      <Avatar size={120} />
+      <Avatar size={120} className="hidden sm:block" />
     </div>
     <small className="font-semibold">Copyright &copy; shio all right reserved.</small>
   </footer>
