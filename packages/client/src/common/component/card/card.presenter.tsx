c53d7126cd3269d5580df7329f9bca10d97caeb8
shio <noir3616@gmail.com>
2022-12-30 18:08:39 +0000

    fix: 🐞 (card) propsの型をexportするように修正

--- packages/client/src/common/component/card/card.presenter.tsx
+++ packages/client/src/common/component/card/card.presenter.tsx
@@ -1,7 +1,7 @@
 import type { ComponentPropsWithoutRef, FC } from 'react';
 import { twMerge } from '@/util/tw-merge';
 
-type CardProps = ComponentPropsWithoutRef<'div'>;
+export type CardProps = ComponentPropsWithoutRef<'div'>;
 
 export const Card: FC<CardProps> = ({ className, children, ...props }) => (
   <div className={twMerge('flex flex-col gap-2 rounded-lg bg-white p-4 shadow', className)} {...props}>
