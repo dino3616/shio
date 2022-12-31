0c762e6ac1d5a2917c4f6780bdd98155544431a4
shio <noir3616@gmail.com>
2022-12-31 09:05:15 +0000

    feat: ✨ (root) Heroがviewportの中央に配置されるように修正

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,10 +1,15 @@
-import type { FC } from 'react';
-import { Introduction } from '@/module/root/ui/introduction.page';
+'use client';
 
-const RootPage: FC = () => (
-  <div>
-    <Introduction className="py-10" />
-  </div>
-);
+import type { FC } from 'react';
+import { useHeaderHeight } from '@/common/state/header.state';
+import { Hero } from '@/module/root/ui/hero.page';
+import { useOptimizeHeroHeight } from '@/module/root/use-case/optimize-hero-height.use-case';
+
+const RootPage: FC = () => {
+  const headerHeight = useHeaderHeight();
+  const heroRef = useOptimizeHeroHeight<HTMLDivElement>(headerHeight);
+
+  return <Hero ref={heroRef} className="py-10" />;
+};
 
 export default RootPage;
