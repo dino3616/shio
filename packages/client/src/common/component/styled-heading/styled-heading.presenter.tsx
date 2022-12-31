e56a6a43e8097c4bd1b15d93880cfbf4019a756e
shio <noir3616@gmail.com>
2022-12-31 18:25:45 +0000

    feat: ✨ (styled-heading) レスポンシブに対応

--- packages/client/src/common/component/styled-heading/styled-heading.presenter.tsx
+++ packages/client/src/common/component/styled-heading/styled-heading.presenter.tsx
@@ -7,9 +7,9 @@ export type StyledHeadingProps = Omit<ComponentPropsWithoutRef<'span'>, 'childre
 };
 
 export const StyledHeading: FC<StyledHeadingProps> = ({ text, alt, className, ...props }) => (
-  <span className={twMerge('font-rakkas text-5xl relative', className)} {...props}>
-    <span className="pr-3 text-8xl">{text.at(0)}</span>
-    <span className="absolute bottom-10">{text.slice(1)}</span>
-    <span className="text-xl font-bold">{alt}</span>
+  <span className={twMerge('font-rakkas text-xl relative md:text-5xl', className)} {...props}>
+    <span className="pr-3 text-7xl md:text-8xl">{text.at(0)}</span>
+    <span className="absolute bottom-6 md:bottom-10">{text.slice(1)}</span>
+    <span className="text-sm font-bold md:text-xl">{alt}</span>
   </span>
 );
--- packages/client/src/common/component/styled-heading/styled-heading.story.tsx
+++ packages/client/src/common/component/styled-heading/styled-heading.story.tsx
@@ -7,7 +7,16 @@ type Story = ComponentStoryObj<typeof StyledHeading>;
 const meta: ComponentMeta<typeof StyledHeading> = {
   component: StyledHeading,
   argTypes: {
+    text: {
+      description: 'main text.',
+      control: { type: 'text' },
+    },
+    alt: {
+      description: 'sub text.',
+      control: { type: 'text' },
+    },
     className: {
+      description: 'class to inject.',
       control: { type: 'text' },
     },
   },
