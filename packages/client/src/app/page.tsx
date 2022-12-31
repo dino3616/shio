0947fcc843fc73292f628eaa8347ded1a3874325
shio <noir3616@gmail.com>
2022-12-31 09:49:48 +0000

    fix: 🐛 (hero) viewportが切り替わるとスタイルが崩れるバグを修正

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -3,13 +3,19 @@
 import type { FC } from 'react';
 import { useHeaderHeight } from '@/common/state/header.state';
 import { Hero } from '@/module/root/ui/hero.page';
+import { Timeline } from '@/module/root/ui/timeline.page';
 import { useOptimizeHeroHeight } from '@/module/root/use-case/optimize-hero-height.use-case';
 
 const RootPage: FC = () => {
   const headerHeight = useHeaderHeight();
   const heroRef = useOptimizeHeroHeight<HTMLDivElement>(headerHeight);
 
-  return <Hero ref={heroRef} className="py-10" />;
+  return (
+    <div className="px-10">
+      <Hero ref={heroRef} />
+      <Timeline />
+    </div>
+  );
 };
 
 export default RootPage;
--- packages/client/src/module/root/ui/hero.page.tsx
+++ packages/client/src/module/root/ui/hero.page.tsx
@@ -6,11 +6,11 @@ import { twMerge } from '@/util/tw-merge';
 export type HeroProps = Omit<ComponentPropsWithRef<'div'>, 'children'>;
 
 export const Hero: FC<HeroProps> = forwardRef<HTMLDivElement, HeroProps>(({ className, ...props }, ref) => (
-  <div ref={ref} className={twMerge('flex items-center justify-center', className)} {...props}>
-    <div className="flex flex-col items-center space-y-10 px-5">
+  <div ref={ref} className={twMerge('flex items-center justify-center gap-10', className)} {...props}>
+    <div className="flex max-w-lg flex-col items-center space-y-10">
       <Avatar size={120} />
       <h1 className="font-rakkas text-5xl underline">Shio</h1>
-      <p className="text-lg font-semibold">フロント・バックエンドエンジニア。クリエイターのクリエイターになることを夢見てる。</p>
+      <p className="font-rakkas text-lg font-semibold">フロント・バックエンドエンジニア。クリエイターのクリエイターになることを夢見てる。</p>
     </div>
     <CodeBox className="hidden lg:block" />
   </div>
