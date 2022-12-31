2a0e52e91db5aa34b860262fcba196af7df7fb1b
shio <noir3616@gmail.com>
2022-12-31 18:23:27 +0000

    doc: 📝 (avatar) sizeの説明を追加

--- packages/client/src/common/component/avatar/avatar.story.tsx
+++ packages/client/src/common/component/avatar/avatar.story.tsx
@@ -7,7 +7,12 @@ type Story = ComponentStoryObj<typeof Avatar>;
 const meta: ComponentMeta<typeof Avatar> = {
   component: Avatar,
   argTypes: {
+    size: {
+      description: 'property that summarizes the height and width properties of next/image',
+      control: { type: 'range', min: 0, max: 255, step: 5 },
+    },
     className: {
+      description: 'class to inject.',
       control: { type: 'text' },
     },
   },
