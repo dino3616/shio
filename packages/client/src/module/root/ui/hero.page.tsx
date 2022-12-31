a793a24d0edfee6a0057b9b46ed677335337ce1e
shio <noir3616@gmail.com>
2022-12-31 10:46:06 +0000

    feat: ✨ (hero) avatarにdrop-shadowを追加

--- packages/client/src/module/root/ui/hero.page.tsx
+++ packages/client/src/module/root/ui/hero.page.tsx
@@ -8,7 +8,7 @@ export type HeroProps = Omit<ComponentPropsWithRef<'div'>, 'children'>;
 export const Hero: FC<HeroProps> = forwardRef<HTMLDivElement, HeroProps>(({ className, ...props }, ref) => (
   <div ref={ref} className={twMerge('flex items-center justify-center gap-10', className)} {...props}>
     <div className="flex max-w-lg flex-col items-center space-y-10">
-      <Avatar size={120} />
+      <Avatar size={120} className="drop-shadow-xl" />
       <h1 className="font-rakkas text-5xl underline">Shio</h1>
       <p className="font-rakkas text-lg font-semibold">フロント・バックエンドエンジニア。クリエイターのクリエイターになることを夢見てる。</p>
     </div>
