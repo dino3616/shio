d6aa1d9be3ccbd8d1c9e940e864049bd54ac858f
shio <noir3616@gmail.com>
2022-12-31 09:48:06 +0000

    fix: 🐞 (code-box) 水平パディングを調整

--- packages/client/src/module/root/ui/component/code-box.presenter.tsx
+++ packages/client/src/module/root/ui/component/code-box.presenter.tsx
@@ -14,7 +14,7 @@ export const CodeBox: FC<CodeBoxProps> = ({ className, ...props }) => (
     className={twMerge('relative flex h-fit w-fit', className)}
     {...props}
   >
-    <div className="rounded-3xl bg-gray-700 p-5 text-left font-code transition hover:-translate-y-1 hover:-translate-x-1 active:translate-y-5 active:translate-x-5">
+    <div className="rounded-3xl bg-gray-700 py-5 px-10 text-left font-code transition hover:-translate-y-1 hover:-translate-x-1 active:translate-y-5 active:translate-x-5">
       <div className="text-info-100 md:text-xl">
         <pre>
           <code>$ shio --introduce</code>
