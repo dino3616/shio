9e7b16d3d5dfdc697f866e0c3e6794aa5b3f0dce
shio <noir3616@gmail.com>
2023-01-01 09:02:30 +0000

    chore: 🔧 (lint-staged) 引数に型注釈を追加

--- packages/api/.lintstagedrc.js
+++ packages/api/.lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
-  '**/*.{js,ts,json}': (filenames) => `yarn eslint --fix ${filenames.join(' --fix ')}`,
-  '**/*.{js,ts,json}': (filenames) => `yarn prettier --check ${filenames.join(' --check ')} --write ${filenames.join(' --write ')}`,
+  '**/*.{js,ts,json}': (/** @type {string[]} */ filenames) => `yarn eslint --fix ${filenames.join(' --fix ')}`,
+  '**/*.{js,ts,json}': (/** @type {string[]} */ filenames) =>
+    `yarn prettier --check ${filenames.join(' --check ')} --write ${filenames.join(' --write ')}`,
 };
--- packages/client/.lintstagedrc.js
+++ packages/client/.lintstagedrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  '**/*.{js,ts,tsx,json}': (filenames) => `yarn eslint --fix ${filenames.join(' --fix ')}`,
-  '**/*.{js,ts,tsx,json}': (filenames) => `yarn prettier --check ${filenames.join(' --check ')} --write ${filenames.join(' --write ')}`,
-  '**/*.{css,scss}': `yarn stylelint --fix ${filenames.join(' --fix ')}`,
+  '**/*.{js,ts,tsx,json}': (/** @type {string[]} */ filenames) => `yarn eslint --fix ${filenames.join(' --fix ')}`,
+  '**/*.{js,ts,tsx,json}': (/** @type {string[]} */ filenames) =>
+    `yarn prettier --check ${filenames.join(' --check ')} --write ${filenames.join(' --write ')}`,
+  '**/*.{css,scss}': (/** @type {string[]} */ filenames) => `yarn stylelint --fix ${filenames.join(' --fix ')}`,
 };
