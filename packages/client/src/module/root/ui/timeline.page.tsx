9ef6665d3b72818a8bb4a9d138fd5594ff130209
shio <85730998+dino3616@users.noreply.github.com>
2023-01-03 06:22:33 +0000

    fix: 🐞 (root/timeline) lineのスタイルを修正

--- packages/client/src/module/root/ui/timeline.page.tsx
+++ packages/client/src/module/root/ui/timeline.page.tsx
@@ -16,12 +16,12 @@ export const Timeline: FC<TimelineProps> = ({ timelines, dateFormatter, classNam
     <div className="relative mt-6">
       <div
         aria-hidden
-        className="absolute inset-0 w-0.5 bg-gray-600 before:absolute before:top-[-6px] before:left-[-5px] before:flex before:h-3 before:w-3 before:rounded-full before:border-4 before:border-solid before:border-gray-600 before:bg-gray-600 after:absolute after:bottom-[-6px] after:left-[-5px] after:flex after:h-3 after:w-3 after:rounded-full after:border-4 after:border-solid after:border-gray-600 after:bg-gray-600"
+        className="absolute inset-0 mx-[5px] w-0.5 bg-gray-600 before:absolute before:top-[-6px] before:left-[-5px] before:flex before:h-3 before:w-3 before:rounded-full before:border-4 before:border-solid before:border-gray-600 before:bg-gray-600 after:absolute after:bottom-[-6px] after:left-[-5px] after:flex after:h-3 after:w-3 after:rounded-full after:border-4 after:border-solid after:border-gray-600 after:bg-gray-600"
       />
       <div className="space-y-5 py-5">
         {timelines.map((timeline) => (
           <ScrollRevealContainer key={timeline.id} duration={0.8} delay={0} distance="40px">
-            <TimelineItem timeline={timeline} dateFormatter={dateFormatter} className="ml-5" />
+            <TimelineItem timeline={timeline} dateFormatter={dateFormatter} className="ml-7" />
           </ScrollRevealContainer>
         ))}
       </div>
