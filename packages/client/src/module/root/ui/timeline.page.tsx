02018098f6cab0fba02cb4e114eb327925341aa6
shio <85730998+dino3616@users.noreply.github.com>
2023-01-03 20:59:46 +0000

    fix: 🐛 (root/timeline) storybookがreact.useに未対応であるため、DIをするように修正

--- packages/client/src/module/root/ui/timeline.page.tsx
+++ packages/client/src/module/root/ui/timeline.page.tsx
@@ -1,4 +1,5 @@
 import { ComponentPropsWithoutRef, FC, Suspense, use } from 'react';
+import { di } from 'react-magnetic-di';
 import { SkeletonTimelineItem, TimelineItem, TimelineItemProps } from './component/timeline-item.presenter';
 import { ScrollRevealContainer } from '@/common/component/scroll-reveal-container/scroll-reveal-container.presenter';
 import { StyledHeading } from '@/common/component/styled-heading/styled-heading.presenter';
@@ -10,23 +11,28 @@ export type TimelineProps = Omit<ComponentPropsWithoutRef<'div'>, 'children'> & 
   dateFormatter: TimelineItemProps['dateFormatter'];
 };
 
-export const Timeline: FC<TimelineProps> = ({ timelinesFinder, dateFormatter, className, ...props }) => (
-  <div className={twMerge('py-10', className)} {...props}>
-    <StyledHeading text="Timeline" alt="タイムライン" />
-    <div className="relative mt-6">
-      <div
-        aria-hidden
-        className="absolute inset-0 mx-[5px] w-0.5 bg-gray-600 before:absolute before:top-[-6px] before:left-[-5px] before:flex before:h-3 before:w-3 before:rounded-full before:border-4 before:border-solid before:border-gray-600 before:bg-gray-600 after:absolute after:bottom-[-6px] after:left-[-5px] after:flex after:h-3 after:w-3 after:rounded-full after:border-4 after:border-solid after:border-gray-600 after:bg-gray-600"
-      />
-      <div className="space-y-5 py-5">
-        <Suspense fallback={<SkeletonTimelineItem className="ml-7" />}>
-          {use(timelinesFinder()).map((timeline) => (
-            <ScrollRevealContainer key={timeline.id} duration={0.8} delay={0} distance="40px">
-              <TimelineItem timeline={timeline} dateFormatter={dateFormatter} className="ml-7" />
-            </ScrollRevealContainer>
-          ))}
-        </Suspense>
+export const Timeline: FC<TimelineProps> = ({ timelinesFinder, dateFormatter, className, ...props }) => {
+  // TODO: Since the storybook does not support `use`, `use` is replaced by `di`. Therefore, as soon as the storybook supports it, `use` will be used directly.
+  di(use);
+
+  return (
+    <div className={twMerge('py-10', className)} {...props}>
+      <StyledHeading text="Timeline" alt="タイムライン" />
+      <div className="relative mt-6">
+        <div
+          aria-hidden
+          className="absolute inset-0 mx-[5px] w-0.5 bg-gray-600 before:absolute before:top-[-6px] before:left-[-5px] before:flex before:h-3 before:w-3 before:rounded-full before:border-4 before:border-solid before:border-gray-600 before:bg-gray-600 after:absolute after:bottom-[-6px] after:left-[-5px] after:flex after:h-3 after:w-3 after:rounded-full after:border-4 after:border-solid after:border-gray-600 after:bg-gray-600"
+        />
+        <div className="space-y-5 py-5">
+          <Suspense fallback={<SkeletonTimelineItem className="ml-7" />}>
+            {use(timelinesFinder()).map((timeline) => (
+              <ScrollRevealContainer key={timeline.id} duration={0.8} delay={0} distance="40px">
+                <TimelineItem timeline={timeline} dateFormatter={dateFormatter} className="ml-7" />
+              </ScrollRevealContainer>
+            ))}
+          </Suspense>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
--- packages/client/src/module/root/ui/timeline.story.tsx
+++ packages/client/src/module/root/ui/timeline.story.tsx
@@ -1,11 +1,29 @@
 import type { ComponentStoryObj, ComponentMeta } from '@storybook/react';
-
+import { use } from 'react';
+import { DiProvider, injectable } from 'react-magnetic-di';
 import { Timeline } from './timeline.page';
+import type { Timeline as TimelineModel } from '@/model/timeline.model';
 
 type Story = ComponentStoryObj<typeof Timeline>;
 
+const injectHooks = () => [
+  injectable<typeof use<TimelineModel[]>>(use, () => [
+    {
+      id: 'abc-123',
+      title: 'hoge event',
+      happenedAt: new Date('1970/01/01'),
+    },
+    {
+      id: 'abc-456',
+      title: 'fuga event',
+      happenedAt: new Date('1970/01/01'),
+    },
+  ]),
+];
+
 const meta: ComponentMeta<typeof Timeline> = {
   component: Timeline,
+  decorators: [(story) => <DiProvider use={injectHooks()}>{story()}</DiProvider>],
   argTypes: {
     className: {
       description: 'class to inject.',
