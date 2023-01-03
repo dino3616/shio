6a05b8d7df031a2bdc59a99410dca0bacc0a0c0b
shio <85730998+dino3616@users.noreply.github.com>
2023-01-03 08:39:32 +0000

    fix: 🐞 (root/timeline) timelinesFinderを外部から注入するように修正

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -2,36 +2,15 @@
 
 import type { FC } from 'react';
 import { formatDate } from '@/infra/date/date.service';
-import { useFindTimelines } from '@/module/root/repository/find-timelines.repository';
+import { findTimelines } from '@/module/root/repository/find-timelines.repository';
 import { Hero } from '@/module/root/ui/hero.page';
 import { Timeline } from '@/module/root/ui/timeline.page';
 
-const RootPage: FC = () => {
-  const timelines = useFindTimelines();
-
-  if (timelines === null) {
-    return null;
-  }
-
-  return (
-    <div className="px-10">
-      <Hero className="h-[calc(100vh_-_76px)]" />
-      {timelines === undefined ? (
-        <Timeline
-          timelines={[
-            {
-              id: '1',
-              title: 'loading...',
-              happenedAt: new Date('1970-01-01T00:00:00.000Z'),
-            },
-          ]}
-          dateFormatter={() => ''}
-        />
-      ) : (
-        <Timeline timelines={timelines} dateFormatter={formatDate} />
-      )}
-    </div>
-  );
-};
+const RootPage: FC = () => (
+  <div className="px-10">
+    <Hero className="h-[calc(100vh_-_76px)]" />
+    <Timeline timelinesFinder={findTimelines} dateFormatter={formatDate} />
+  </div>
+);
 
 export default RootPage;
--- packages/client/src/module/root/ui/timeline.page.tsx
+++ packages/client/src/module/root/ui/timeline.page.tsx
@@ -1,16 +1,16 @@
-import type { ComponentPropsWithoutRef, FC } from 'react';
-import { TimelineItem, TimelineItemProps } from './component/timeline-item.presenter';
+import { ComponentPropsWithoutRef, FC, Suspense, use } from 'react';
+import { SkeletonTimelineItem, TimelineItem, TimelineItemProps } from './component/timeline-item.presenter';
 import { ScrollRevealContainer } from '@/common/component/scroll-reveal-container/scroll-reveal-container.presenter';
 import { StyledHeading } from '@/common/component/styled-heading/styled-heading.presenter';
 import type { Timeline as TimelineModel } from '@/model/timeline.model';
 import { twMerge } from '@/util/tw-merge';
 
 export type TimelineProps = Omit<ComponentPropsWithoutRef<'div'>, 'children'> & {
-  timelines: TimelineModel[];
+  timelinesFinder: () => Promise<TimelineModel[]>;
   dateFormatter: TimelineItemProps['dateFormatter'];
 };
 
-export const Timeline: FC<TimelineProps> = ({ timelines, dateFormatter, className, ...props }) => (
+export const Timeline: FC<TimelineProps> = ({ timelinesFinder: findTimelines, dateFormatter, className, ...props }) => (
   <div className={twMerge('py-10', className)} {...props}>
     <StyledHeading text="Timeline" alt="タイムライン" />
     <div className="relative mt-6">
@@ -19,11 +19,13 @@ export const Timeline: FC<TimelineProps> = ({ timelines, dateFormatter, classNam
         className="absolute inset-0 mx-[5px] w-0.5 bg-gray-600 before:absolute before:top-[-6px] before:left-[-5px] before:flex before:h-3 before:w-3 before:rounded-full before:border-4 before:border-solid before:border-gray-600 before:bg-gray-600 after:absolute after:bottom-[-6px] after:left-[-5px] after:flex after:h-3 after:w-3 after:rounded-full after:border-4 after:border-solid after:border-gray-600 after:bg-gray-600"
       />
       <div className="space-y-5 py-5">
-        {timelines.map((timeline) => (
-          <ScrollRevealContainer key={timeline.id} duration={0.8} delay={0} distance="40px">
-            <TimelineItem timeline={timeline} dateFormatter={dateFormatter} className="ml-7" />
-          </ScrollRevealContainer>
-        ))}
+        <Suspense fallback={<SkeletonTimelineItem className="ml-7" />}>
+          {use(findTimelines()).map((timeline) => (
+            <ScrollRevealContainer key={timeline.id} duration={0.8} delay={0} distance="40px">
+              <TimelineItem timeline={timeline} dateFormatter={dateFormatter} className="ml-7" />
+            </ScrollRevealContainer>
+          ))}
+        </Suspense>
       </div>
     </div>
   </div>
