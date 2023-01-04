f0365d12ffdce7734d3e43b0cf67435090fd5a65
shio <85730998+dino3616@users.noreply.github.com>
2023-01-04 19:09:01 +0000

    fix: 🐛 (root) root-pageをCC化

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import type { FC } from 'react';
 import { formatDate } from '@/infra/date/date.service';
 import { useFindTimelines } from '@/module/root/repository/find-timelines.repository';
