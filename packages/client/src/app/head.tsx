47f2bce6b94a98fce2bcc8e7852f08e48f1d8946
shio <85730998+dino3616@users.noreply.github.com>
2023-01-04 07:39:24 +0000

    fix: 🐞 (root/head) titleのタイポミスを修正

--- packages/client/src/app/head.tsx
+++ packages/client/src/app/head.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react';
 
 const RootHead: FC = () => (
   <>
-    <title>shio.dev | Creators fot Creator</title>
+    <title>shio.dev | Creator for Creators</title>
     <meta content="width=device-width, initial-scale=1" name="viewport" />
     <link rel="icon" href="/favicon.png" />
   </>
