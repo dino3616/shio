190fdd11b3892dcd5868c0881dd6d25dc1b16976
shio <85730998+dino3616@users.noreply.github.com>
2023-01-04 18:58:18 +0000

    feat: ⚰️ (root/timeline) storyの不要なinjectionを削除

--- packages/client/src/module/root/ui/timeline.story.tsx
+++ packages/client/src/module/root/ui/timeline.story.tsx
@@ -1,29 +1,10 @@
 import type { ComponentStoryObj, ComponentMeta } from '@storybook/react';
-import { use } from 'react';
-import { DiProvider, injectable } from 'react-magnetic-di';
 import { Timeline } from './timeline.page';
-import type { Timeline as TimelineModel } from '@/model/timeline.model';
 
 type Story = ComponentStoryObj<typeof Timeline>;
 
-const injectHooks = () => [
-  injectable<typeof use<TimelineModel[]>>(use, () => [
-    {
-      id: 'abc-123',
-      title: 'hoge event',
-      happenedAt: new Date('1970/01/01'),
-    },
-    {
-      id: 'abc-456',
-      title: 'fuga event',
-      happenedAt: new Date('1970/01/01'),
-    },
-  ]),
-];
-
 const meta: ComponentMeta<typeof Timeline> = {
   component: Timeline,
-  decorators: [(story) => <DiProvider use={injectHooks()}>{story()}</DiProvider>],
   argTypes: {
     className: {
       description: 'class to inject.',
@@ -36,18 +17,20 @@ export default meta;
 
 export const Default: Story = {
   args: {
-    timelinesFinder: async () => [
-      {
-        id: 'abc-123',
-        title: 'hoge event',
-        happenedAt: new Date('1970/01/01'),
-      },
-      {
-        id: 'abc-456',
-        title: 'fuga event',
-        happenedAt: new Date('1970/01/01'),
-      },
-    ],
-    dateFormatter: () => 'Jan. 01 1970',
+    foundTimelinesProps: {
+      useFindTimelines: () => [
+        {
+          id: 'abc-123',
+          title: 'hoge event',
+          happenedAt: new Date('1970/01/01'),
+        },
+        {
+          id: 'abc-456',
+          title: 'fuga event',
+          happenedAt: new Date('1970/01/01'),
+        },
+      ],
+      formatDate: () => 'Jan. 01 1970',
+    },
   },
 };
