9e25ea8bf779bbea2f52bec65ab8127188ddad0d
shio <85730998+dino3616@users.noreply.github.com>
2023-01-04 19:04:28 +0000

    fix: 🐞 (root) useではなくthrow promiseによってsuspendするように修正

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,15 +1,18 @@
-'use client';
-
 import type { FC } from 'react';
 import { formatDate } from '@/infra/date/date.service';
-import { findTimelines } from '@/module/root/repository/find-timelines.repository';
+import { useFindTimelines } from '@/module/root/repository/find-timelines.repository';
 import { Hero } from '@/module/root/ui/hero.page';
 import { Timeline } from '@/module/root/ui/timeline.page';
 
 const RootPage: FC = () => (
   <div className="p-10 pt-0">
     <Hero className="h-[calc(100vh_-_4.75rem)]" />
-    <Timeline timelinesFinder={findTimelines} dateFormatter={formatDate} />
+    <Timeline
+      foundTimelinesProps={{
+        useFindTimelines,
+        formatDate,
+      }}
+    />
   </div>
 );
 
--- packages/client/src/module/root/ui/timeline.page.tsx
+++ packages/client/src/module/root/ui/timeline.page.tsx
@@ -1,39 +1,28 @@
 'use client';
 
-import { ComponentPropsWithoutRef, FC, Suspense, use } from 'react';
-import { di } from 'react-magnetic-di';
-import { SkeletonTimelineItem, TimelineItem, TimelineItemProps } from './component/timeline-item.presenter';
-import { ScrollRevealContainer } from '@/common/component/scroll-reveal-container/scroll-reveal-container.presenter';
+import { ComponentPropsWithoutRef, FC, Suspense } from 'react';
+import { SkeletonTimeline } from '../domain/timeline/component/timeline.presenter';
+import { FoundTimelines, FoundTimelinesProps } from '../use-case/found-timelines.use-case';
 import { StyledHeading } from '@/common/component/styled-heading/styled-heading.presenter';
-import type { Timeline as TimelineModel } from '@/model/timeline.model';
+import { twMerge } from '@/common/util/tw-merge.util';
 
 export type TimelineProps = Omit<ComponentPropsWithoutRef<'div'>, 'children'> & {
-  timelinesFinder: () => Promise<TimelineModel[]>;
-  dateFormatter: TimelineItemProps['dateFormatter'];
+  foundTimelinesProps: FoundTimelinesProps;
 };
 
-export const Timeline: FC<TimelineProps> = ({ timelinesFinder, dateFormatter, className, ...props }) => {
-  // TODO: Since the storybook does not support `use`, `use` is replaced by `di`. Therefore, as soon as the storybook supports it, `use` will be used directly.
-  di(use);
-
-  return (
-    <div className={className} {...props}>
-      <StyledHeading text="Timeline" alt="タイムライン" />
-      <div className="relative mt-6">
-        <div
-          aria-hidden
-          className="absolute inset-0 m-[5px] w-0.5 bg-gray-600 before:absolute before:top-[-5px] before:left-[-5px] before:flex before:h-3 before:w-3 before:rounded-full before:border-4 before:border-solid before:border-gray-600 before:bg-gray-600 after:absolute after:bottom-[-5px] after:left-[-5px] after:flex after:h-3 after:w-3 after:rounded-full after:border-4 after:border-solid after:border-gray-600 after:bg-gray-600"
-        />
-        <div className="space-y-5 py-5">
-          <Suspense fallback={<SkeletonTimelineItem className="ml-7" />}>
-            {use(timelinesFinder()).map((timeline) => (
-              <ScrollRevealContainer key={timeline.id} duration={0.8} delay={0} distance="40px">
-                <TimelineItem timeline={timeline} dateFormatter={dateFormatter} className="ml-7" />
-              </ScrollRevealContainer>
-            ))}
-          </Suspense>
-        </div>
+export const Timeline: FC<TimelineProps> = ({ foundTimelinesProps, ...props }) => (
+  <div {...props}>
+    <StyledHeading text="Timeline" alt="タイムライン" />
+    <div className="relative mt-6">
+      <div
+        aria-hidden
+        className="absolute inset-0 m-[5px] w-0.5 bg-gray-600 before:absolute before:top-[-5px] before:left-[-5px] before:flex before:h-3 before:w-3 before:rounded-full before:border-4 before:border-solid before:border-gray-600 before:bg-gray-600 after:absolute after:bottom-[-5px] after:left-[-5px] after:flex after:h-3 after:w-3 after:rounded-full after:border-4 after:border-solid after:border-gray-600 after:bg-gray-600"
+      />
+      <div className="space-y-5 py-5">
+        <Suspense fallback={<SkeletonTimeline className="ml-7" />}>
+          <FoundTimelines className={twMerge('ml-7', foundTimelinesProps.className)} {...foundTimelinesProps} />
+        </Suspense>
       </div>
     </div>
-  );
-};
+  </div>
+);
