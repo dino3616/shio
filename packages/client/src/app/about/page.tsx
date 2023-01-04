e0f0017628a1e026f5d78bdeec8c4f429cbe449e
shio <85730998+dino3616@users.noreply.github.com>
2023-01-04 19:05:32 +0000

    feat: ✨ (about) aboutページを作成

--- packages/client/src/app/about/page.tsx
+++ packages/client/src/app/about/page.tsx
@@ -1,5 +1,12 @@
 import type { FC } from 'react';
+import { Outline } from '@/module/about/ui/outline.page';
+import { SnsList } from '@/module/about/ui/sns-list.page';
 
-const AboutPage: FC = () => <div>about</div>;
+const AboutPage: FC = () => (
+  <div className="space-y-5 p-10">
+    <Outline className="border-b-2 border-gray-400 pb-10" />
+    <SnsList className="" />
+  </div>
+);
 
 export default AboutPage;
