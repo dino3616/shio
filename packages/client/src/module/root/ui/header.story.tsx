d090f3ebcc174c746ed688286bd0917aaeec9b87
shio <85730998+dino3616@users.noreply.github.com>
2023-01-05 00:23:02 +0000

    fix: 🐞 (root/header) storyの不要なRecoilRootを削除

--- packages/client/src/module/root/ui/header.story.tsx
+++ packages/client/src/module/root/ui/header.story.tsx
@@ -1,5 +1,4 @@
 import type { ComponentStoryObj, ComponentMeta } from '@storybook/react';
-import { RecoilRoot } from 'recoil';
 
 import { Header } from './header.layout';
 
@@ -7,7 +6,6 @@ type Story = ComponentStoryObj<typeof Header>;
 
 const meta: ComponentMeta<typeof Header> = {
   component: Header,
-  decorators: [(story) => <RecoilRoot>{story()}</RecoilRoot>],
   argTypes: {
     className: {
       description: 'class to inject.',
