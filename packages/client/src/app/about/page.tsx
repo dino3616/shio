abfcd650968ec30cb3bf8fd9c869a5a6f917e53d
shio <85730998+dino3616@users.noreply.github.com>
2023-01-05 00:18:18 +0000

    fix: 🐞 (about) sns-listのjustify-contentがendになるように修正

--- packages/client/src/app/about/page.tsx
+++ packages/client/src/app/about/page.tsx
@@ -5,7 +5,7 @@ import { SnsList } from '@/module/about/ui/sns-list.page';
 const AboutPage: FC = () => (
   <div className="space-y-5 p-10">
     <Outline className="border-b-2 border-gray-400 pb-10" />
-    <SnsList className="" />
+    <SnsList className="flex justify-end" />
   </div>
 );
 
