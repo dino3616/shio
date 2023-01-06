d7029cb1767653095d2c84473e5f5edf0f48ef8f
shio <85730998+dino3616@users.noreply.github.com>
2023-01-06 16:53:13 +0000

    feat: ✨ (root) navigationにnavigation-menuを使用するように修正

--- packages/client/src/module/root/ui/component/hamburger-menu.presenter.tsx
+++ packages/client/src/module/root/ui/component/hamburger-menu.presenter.tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link';
 import type { ComponentPropsWithoutRef, FC } from 'react';
 import { FaHamburger } from 'react-icons/fa';
 import type { IconBaseProps } from 'react-icons/lib';
 import { Button } from '@/common/component/button/button.presenter';
 import { Dialog } from '@/common/component/dialog/dialog.presenter';
+import { NavigationMenu } from '@/common/component/navigation-menu/navigation-menu.presenter';
 
 export type HamburgerMenuProps = Omit<ComponentPropsWithoutRef<typeof Dialog.Root>, 'children'> & {
   iconSize: Required<IconBaseProps['size']>;
@@ -19,22 +19,22 @@ export const HamburgerMenu: FC<HamburgerMenuProps> = ({ iconSize, ...props }) =>
     <Dialog.Portal>
       <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 space-y-7 rounded-xl border-2 border-white/40 bg-white/10 px-5 py-7 drop-shadow-lg backdrop-blur-lg">
         <Dialog.Title className="mx-5 text-5xl font-bold">shio.dev</Dialog.Title>
-        <nav>
-          <ul className="flex flex-col items-center justify-center space-y-5 text-2xl font-semibold underline">
-            <li>
-              <Link href="/product">Product</Link>
-            </li>
-            <li>
-              <Link href="/blog">Blog</Link>
-            </li>
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-            <li>
-              <Link href="/skill">Skill</Link>
-            </li>
-          </ul>
-        </nav>
+        <NavigationMenu.Root>
+          <NavigationMenu.List className="flex flex-col items-center justify-center space-y-5 text-2xl font-semibold underline">
+            <NavigationMenu.Item>
+              <NavigationMenu.Link href="/product">Product</NavigationMenu.Link>
+            </NavigationMenu.Item>
+            <NavigationMenu.Item>
+              <NavigationMenu.Link href="/blog">Blog</NavigationMenu.Link>
+            </NavigationMenu.Item>
+            <NavigationMenu.Item>
+              <NavigationMenu.Link href="/about">About</NavigationMenu.Link>
+            </NavigationMenu.Item>
+            <NavigationMenu.Item>
+              <NavigationMenu.Link href="/skill">Skill</NavigationMenu.Link>
+            </NavigationMenu.Item>
+          </NavigationMenu.List>
+        </NavigationMenu.Root>
       </Dialog.Content>
     </Dialog.Portal>
   </Dialog.Root>
--- packages/client/src/module/root/ui/header.layout.tsx
+++ packages/client/src/module/root/ui/header.layout.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
 import type { ComponentPropsWithoutRef, FC } from 'react';
 import { HamburgerMenu } from './component/hamburger-menu.presenter';
+import { NavigationMenu } from '@/common/component/navigation-menu/navigation-menu.presenter';
 import { twMerge } from '@/common/util/tw-merge.util';
 
 export type HeaderProps = Omit<ComponentPropsWithoutRef<'header'>, 'children'>;
@@ -10,35 +10,35 @@ export const Header: FC<HeaderProps> = ({ className, ...props }) => (
     className={twMerge('z-50 bg-white py-6 px-5 font-inter shadow-lg justify-between md:justify-center flex items-center', className)}
     {...props}
   >
-    <nav>
-      <ul className="flex items-center justify-start md:justify-center md:space-x-7">
-        <li>
-          <Link href="/product" className="hidden transition-colors hover:text-gray-400 md:block">
+    <NavigationMenu.Root>
+      <NavigationMenu.List className="flex items-center justify-start md:justify-center md:space-x-7">
+        <NavigationMenu.Item>
+          <NavigationMenu.Link href="/product" className="hidden transition-colors hover:text-gray-400 md:block">
             Product
-          </Link>
-        </li>
-        <li>
-          <Link href="/blog" className="hidden transition-colors hover:text-gray-400 md:block">
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+          <NavigationMenu.Link href="/blog" className="hidden transition-colors hover:text-gray-400 md:block">
             Blog
-          </Link>
-        </li>
-        <li>
-          <Link href="/" className="text-lg font-bold uppercase tracking-wide">
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+          <NavigationMenu.Link href="/" className="text-lg font-bold uppercase tracking-wide">
             Shio.dev
-          </Link>
-        </li>
-        <li>
-          <Link href="/about" className="hidden transition-colors hover:text-gray-400 md:block">
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+          <NavigationMenu.Link href="/about" className="hidden transition-colors hover:text-gray-400 md:block">
             About
-          </Link>
-        </li>
-        <li>
-          <Link href="/skill" className="hidden transition-colors hover:text-gray-400 md:block">
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+          <NavigationMenu.Link href="/skill" className="hidden transition-colors hover:text-gray-400 md:block">
             Skill
-          </Link>
-        </li>
-      </ul>
-    </nav>
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+      </NavigationMenu.List>
+    </NavigationMenu.Root>
     <div className="block md:hidden">
       <HamburgerMenu iconSize={32} />
     </div>
