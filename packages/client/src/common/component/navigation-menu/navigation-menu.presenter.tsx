69b0823e5dc6a1eecf2f9cd29c042d5d13727ae1
shio <85730998+dino3616@users.noreply.github.com>
2023-01-06 17:47:38 +0000

    feat: ✨ (common/navigation-menu) linkにnext/linkを使用するように修正

--- packages/client/src/common/component/navigation-menu/navigation-menu.presenter.tsx
+++ packages/client/src/common/component/navigation-menu/navigation-menu.presenter.tsx
@@ -1,3 +1,21 @@
-import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import * as RadixUiNavigationMenu from '@radix-ui/react-navigation-menu';
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import type { ComponentPropsWithoutRef, FC } from 'react';
 
-export { NavigationMenu };
+type LinkProps = ComponentPropsWithoutRef<typeof RadixUiNavigationMenu.Link> & {
+  href: string;
+};
+
+const Link: FC<LinkProps> = ({ href, ...props }) => {
+  const router = useRouter();
+  const isActive = router.asPath === href;
+
+  return (
+    <NextLink href={href} passHref>
+      <RadixUiNavigationMenu.Link active={isActive} {...props} />
+    </NextLink>
+  );
+};
+
+export const NavigationMenu = { ...RadixUiNavigationMenu, Link };
