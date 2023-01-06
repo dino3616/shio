6f21307f8661c705cb140e97f14c456c6e54909f
shio <85730998+dino3616@users.noreply.github.com>
2023-01-06 16:01:47 +0000

    feat: ✨ (root/header) ハンバーガーメニューを実装

--- packages/client/src/module/root/ui/header.layout.tsx
+++ packages/client/src/module/root/ui/header.layout.tsx
@@ -1,37 +1,46 @@
 import Link from 'next/link';
 import type { ComponentPropsWithoutRef, FC } from 'react';
+import { HamburgerMenu } from './component/hamburger-menu.presenter';
 import { twMerge } from '@/common/util/tw-merge.util';
 
 export type HeaderProps = Omit<ComponentPropsWithoutRef<'header'>, 'children'>;
 
 export const Header: FC<HeaderProps> = ({ className, ...props }) => (
-  <header className={twMerge('z-50 bg-white py-6 font-inter shadow-lg', className)} {...props}>
-    <ul className="flex items-center justify-center space-x-7">
-      <li>
-        <Link href="/product" className="transition-colors hover:text-gray-400">
-          Product
-        </Link>
-      </li>
-      <li>
-        <Link href="/blog" className="transition-colors hover:text-gray-400">
-          Blog
-        </Link>
-      </li>
-      <li>
-        <Link href="/" className="text-lg font-semibold uppercase tracking-wide">
-          Shio.dev
-        </Link>
-      </li>
-      <li>
-        <Link href="/about" className="transition-colors hover:text-gray-400">
-          About
-        </Link>
-      </li>
-      <li>
-        <Link href="/skill" className="transition-colors hover:text-gray-400">
-          Skill
-        </Link>
-      </li>
-    </ul>
+  <header
+    className={twMerge('z-50 bg-white py-6 px-5 font-inter shadow-lg justify-between md:justify-center flex items-center', className)}
+    {...props}
+  >
+    <nav>
+      <ul className="flex items-center justify-start md:justify-center md:space-x-7">
+        <li>
+          <Link href="/product" className="hidden transition-colors hover:text-gray-400 md:block">
+            Product
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog" className="hidden transition-colors hover:text-gray-400 md:block">
+            Blog
+          </Link>
+        </li>
+        <li>
+          <Link href="/" className="text-lg font-bold uppercase tracking-wide">
+            Shio.dev
+          </Link>
+        </li>
+        <li>
+          <Link href="/about" className="hidden transition-colors hover:text-gray-400 md:block">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link href="/skill" className="hidden transition-colors hover:text-gray-400 md:block">
+            Skill
+          </Link>
+        </li>
+      </ul>
+    </nav>
+    <div className="block md:hidden">
+      <HamburgerMenu iconSize={32} />
+    </div>
   </header>
 );
