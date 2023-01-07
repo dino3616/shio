d25ff6c65ec3cb2e8507e413399d161bc43835ad
shio <85730998+dino3616@users.noreply.github.com>
2023-01-07 15:26:54 +0000

    fix: 🐞 (root/timeline) timelineをpropsで受け取るように修正

--- packages/client/src/module/root/ui/timeline.page.tsx
+++ packages/client/src/module/root/ui/timeline.page.tsx
@@ -1,14 +1,15 @@
-import { ComponentPropsWithoutRef, FC, Suspense } from 'react';
-import { FoundTimelines, FoundTimelinesProps } from '../use-case/found-timelines.use-case';
-import { SkeletonTimeline } from './component/timeline/timeline.presenter';
+import type { ComponentPropsWithoutRef, FC } from 'react';
+import type { Timeline as TimelineModel } from '../model/timeline.model';
+import { Timeline as TimelineItem } from './component/timeline/timeline.presenter';
 import { Heading } from '@/common/component/heading/heading.presenter';
-import { twMerge } from '@/common/util/tw-merge.util';
+import { ScrollRevealAnimation } from '@/common/component/scroll-reveal-animation/scroll-reveal-animation.presenter';
 
 export type TimelineProps = Omit<ComponentPropsWithoutRef<'div'>, 'children'> & {
-  foundTimelinesProps: FoundTimelinesProps;
+  timelines: TimelineModel[];
+  timelineItemProps: Omit<ComponentPropsWithoutRef<typeof TimelineItem>, 'timeline'>;
 };
 
-export const Timeline: FC<TimelineProps> = ({ foundTimelinesProps, ...props }) => (
+export const Timeline: FC<TimelineProps> = ({ timelines, timelineItemProps, ...props }) => (
   <div {...props}>
     <Heading text="Timeline" alt="タイムライン" />
     <div className="relative mt-6">
@@ -17,9 +18,11 @@ export const Timeline: FC<TimelineProps> = ({ foundTimelinesProps, ...props }) =
         className="absolute inset-0 m-[5px] w-0.5 bg-gray-600 before:absolute before:top-[-5px] before:left-[-5px] before:flex before:h-3 before:w-3 before:rounded-full before:border-4 before:border-solid before:border-gray-600 before:bg-gray-600 after:absolute after:bottom-[-5px] after:left-[-5px] after:flex after:h-3 after:w-3 after:rounded-full after:border-4 after:border-solid after:border-gray-600 after:bg-gray-600"
       />
       <div className="space-y-5 py-5">
-        <Suspense fallback={<SkeletonTimeline className="ml-7" />}>
-          <FoundTimelines className={twMerge('ml-7', foundTimelinesProps.className)} {...foundTimelinesProps} />
-        </Suspense>
+        {timelines.map((timeline) => (
+          <ScrollRevealAnimation key={timeline.id} duration={0.8} delay={0} distance="40px">
+            <TimelineItem className="ml-7" timeline={timeline} {...timelineItemProps} />
+          </ScrollRevealAnimation>
+        ))}
       </div>
     </div>
   </div>
