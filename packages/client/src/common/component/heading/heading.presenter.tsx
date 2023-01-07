85127a5a27dabce7fffb7381605ec478c1800082
shio <85730998+dino3616@users.noreply.github.com>
2023-01-07 09:33:35 +0000

    fix: 🐛 (common/heading) スタイルの崩れを修正

--- packages/client/src/common/component/heading/heading.presenter.tsx
+++ packages/client/src/common/component/heading/heading.presenter.tsx
@@ -7,11 +7,11 @@ export type HeadingProps = Omit<ComponentPropsWithoutRef<'span'>, 'children'> & 
 };
 
 export const Heading: FC<HeadingProps> = ({ text, alt, className, ...props }) => (
-  <span className={twMerge('font-rakkas text-xl relative md:text-5xl', className)} {...props}>
+  <span className={twMerge('relative flex font-rakkas text-4xl md:text-5xl', className)} {...props}>
     <h2>
       <span className="pr-3 text-7xl md:text-8xl">{text.at(0)}</span>
-      <span className="absolute bottom-6 md:bottom-10">{text.slice(1)}</span>
+      <span className="absolute bottom-7 md:bottom-10">{text.slice(1)}</span>
     </h2>
-    <span className="text-sm font-bold md:text-xl">{alt}</span>
+    <div className="mt-auto mb-[0.65rem] inline text-sm font-bold md:mb-[0.8rem] md:text-xl">{alt}</div>
   </span>
 );
