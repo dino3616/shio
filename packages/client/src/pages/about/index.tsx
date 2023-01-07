eaa84a2461af1fc1c3bed773fa8536ae55835145
shio <85730998+dino3616@users.noreply.github.com>
2023-01-07 15:24:53 +0000

    fix: 🐞 (pages/about) 型をFCからNextPageに修正

--- packages/client/src/pages/about/index.tsx
+++ packages/client/src/pages/about/index.tsx
@@ -1,9 +1,9 @@
-import type { FC } from 'react';
+import type { NextPage } from 'next';
 import { Outline } from '@/module/about/ui/outline.page';
 import { SnsList } from '@/module/about/ui/sns-list.page';
 import { Layout } from '@/module/layout/ui/layout.page';
 
-const AboutPage: FC = () => (
+const AboutPage: NextPage = () => (
   <Layout title="About | shio.dev" className="space-y-5 py-10 px-5 md:px-10">
     <Outline className="border-b-2 border-gray-400 pb-10" />
     <SnsList className="flex justify-end" />
