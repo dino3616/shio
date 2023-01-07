805a4b54307f35797cb1e4972179473ad1f104ee
shio <85730998+dino3616@users.noreply.github.com>
2023-01-07 15:26:14 +0000

    fix: 🐞 (root/repository) 非同期に処理を行うように修正

--- packages/client/src/module/root/repository/find-timelines.repository.tsx
+++ packages/client/src/module/root/repository/find-timelines.repository.tsx
@@ -1,15 +1,15 @@
-import type { Timeline } from '../model/timeline.model';
-import { useFindTimelinesQuery } from '@/infra/graphql/generated/graphql';
+import { FindTimelinesDocument, FindTimelinesQuery, FindTimelinesQueryVariables, Timeline } from '@/infra/graphql/generated/graphql';
+import { urqlClient } from '@/infra/urql/urql.service';
 
-export type UseFindTimelinesInterface = () => Timeline[];
+export type FindTimelinesInterface = () => Promise<Timeline[]>;
 
-export const useFindTimelines: UseFindTimelinesInterface = () => {
-  const [{ data, error }] = useFindTimelinesQuery({
-    requestPolicy: 'cache-and-network',
-  });
+export const findTimelines: FindTimelinesInterface = async () => {
+  const { data, error } = await urqlClient
+    .query<FindTimelinesQuery, FindTimelinesQueryVariables>(FindTimelinesDocument, {}, { requestPolicy: 'cache-and-network' })
+    .toPromise();
 
   if (data?.findTimelines === undefined || error) {
-    throw error || new Error(`An error occurred while '${useFindTimelines.name}'.`);
+    throw error || new Error('An error occurred while `findTimelines()`.');
   }
 
   return data.findTimelines;
