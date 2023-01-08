f23498816f6ff656f43e2a159e2d4edf410b3e0d
shio <85730998+dino3616@users.noreply.github.com>
2023-01-08 09:11:26 +0000

    fix: 🐞 (http-error) レスポンシブデザインに対応

--- packages/client/src/module/http-error/ui/http-error.page.tsx
+++ packages/client/src/module/http-error/ui/http-error.page.tsx
@@ -10,7 +10,7 @@ export type HttpErrorProps = Omit<ComponentPropsWithoutRef<'div'>, 'children'> &
 export const HttpError: FC<HttpErrorProps> = ({ title, message, className, ...props }) => (
   <div className={twMerge('flex items-center gap-24', className)} {...props}>
     <Image src="/http-error.svg" alt="avatar" width={320} height={320} />
-    <div>
+    <div className="absolute rounded-xl border-2 border-white/40 bg-white/10 px-5 py-7 drop-shadow-lg backdrop-blur-lg md:static">
       <h1 className="font-rakkas text-5xl">{title}</h1>
       <p className="text-lg">{message}</p>
     </div>
