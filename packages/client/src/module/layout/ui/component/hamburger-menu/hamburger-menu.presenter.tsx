64ae68f3441b701044ce2049f1b4d1d5b00efc8d
shio <85730998+dino3616@users.noreply.github.com>
2023-01-08 05:48:03 +0000

    fix: 🐞 (layout/hamburger-menu) マウント時のアニメーションを追加

--- packages/client/src/module/layout/ui/component/hamburger-menu/hamburger-menu.presenter.tsx
+++ packages/client/src/module/layout/ui/component/hamburger-menu/hamburger-menu.presenter.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 import type { ComponentPropsWithoutRef, Dispatch, FC, SetStateAction } from 'react';
 import { FaHamburger } from 'react-icons/fa';
@@ -23,36 +24,43 @@ export const HamburgerMenu: FC<HamburgerMenuProps> = ({ iconSize, modalControlle
       </Button>
     </Dialog.Trigger>
     <Dialog.Portal>
-      <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 space-y-7 rounded-xl border-2 border-white/40 bg-white/10 px-5 py-7 drop-shadow-lg backdrop-blur-lg">
-        <Dialog.Title className="mx-5 text-5xl font-bold">
-          <Link href="/" onClick={modalController?.toClose}>
-            shio.dev
-          </Link>
-        </Dialog.Title>
-        <NavigationMenu.Root>
-          <NavigationMenu.List className="flex flex-col items-center justify-center space-y-5 text-2xl font-semibold underline">
-            <NavigationMenu.Item>
-              <NavigationMenu.Link href="/product" onClick={modalController?.toClose}>
-                Product
-              </NavigationMenu.Link>
-            </NavigationMenu.Item>
-            <NavigationMenu.Item>
-              <NavigationMenu.Link href="/blog" onClick={modalController?.toClose}>
-                Blog
-              </NavigationMenu.Link>
-            </NavigationMenu.Item>
-            <NavigationMenu.Item>
-              <NavigationMenu.Link href="/about" onClick={modalController?.toClose}>
-                About
-              </NavigationMenu.Link>
-            </NavigationMenu.Item>
-            <NavigationMenu.Item>
-              <NavigationMenu.Link href="/skill" onClick={modalController?.toClose}>
-                Skill
-              </NavigationMenu.Link>
-            </NavigationMenu.Item>
-          </NavigationMenu.List>
-        </NavigationMenu.Root>
+      <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <motion.div
+          initial={{ scale: 0.8 }}
+          animate={{ scale: 1.0 }}
+          exit={{ scale: 0.8 }}
+          className="space-y-7 rounded-xl border-2 border-white/40 bg-white/10 px-5 py-7 drop-shadow-lg backdrop-blur-lg"
+        >
+          <Dialog.Title className="mx-5 text-5xl font-bold">
+            <Link href="/" onClick={modalController?.toClose}>
+              shio.dev
+            </Link>
+          </Dialog.Title>
+          <NavigationMenu.Root>
+            <NavigationMenu.List className="flex flex-col items-center justify-center space-y-5 text-2xl font-semibold underline">
+              <NavigationMenu.Item>
+                <NavigationMenu.Link href="/product" onClick={modalController?.toClose}>
+                  Product
+                </NavigationMenu.Link>
+              </NavigationMenu.Item>
+              <NavigationMenu.Item>
+                <NavigationMenu.Link href="/blog" onClick={modalController?.toClose}>
+                  Blog
+                </NavigationMenu.Link>
+              </NavigationMenu.Item>
+              <NavigationMenu.Item>
+                <NavigationMenu.Link href="/about" onClick={modalController?.toClose}>
+                  About
+                </NavigationMenu.Link>
+              </NavigationMenu.Item>
+              <NavigationMenu.Item>
+                <NavigationMenu.Link href="/skill" onClick={modalController?.toClose}>
+                  Skill
+                </NavigationMenu.Link>
+              </NavigationMenu.Item>
+            </NavigationMenu.List>
+          </NavigationMenu.Root>
+        </motion.div>
       </Dialog.Content>
     </Dialog.Portal>
   </Dialog.Root>
