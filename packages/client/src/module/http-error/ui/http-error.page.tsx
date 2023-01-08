af1f66ce0c6e943dab6d638ef03c82a3e83f663a
shio <85730998+dino3616@users.noreply.github.com>
2023-01-08 09:33:39 +0000

    fix: 🐞 (http-error) cardのpaddingを修正

--- packages/client/src/module/http-error/ui/http-error.page.tsx
+++ packages/client/src/module/http-error/ui/http-error.page.tsx
@@ -8,9 +8,9 @@ export type HttpErrorProps = Omit<ComponentPropsWithoutRef<'div'>, 'children'> &
 };
 
 export const HttpError: FC<HttpErrorProps> = ({ title, message, className, ...props }) => (
-  <div className={twMerge('flex items-center gap-24', className)} {...props}>
+  <div className={twMerge('relative flex items-center gap-24', className)} {...props}>
     <Image src="/http-error.svg" alt="avatar" width={320} height={320} />
-    <div className="absolute rounded-xl border-2 border-white/40 bg-white/10 px-5 py-7 drop-shadow-lg backdrop-blur-lg md:static">
+    <div className="absolute mx-5 rounded-xl border-2 border-white/40 bg-white/10 px-5 py-7 drop-shadow-lg backdrop-blur-lg md:static md:mx-0">
       <h1 className="font-rakkas text-5xl">{title}</h1>
       <p className="text-lg">{message}</p>
     </div>
