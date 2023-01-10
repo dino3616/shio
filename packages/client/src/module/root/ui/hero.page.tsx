71488ea502afb12d25e63e30d19ee7b640213af7
shio <85730998+dino3616@users.noreply.github.com>
2023-01-10 12:25:26 +0000

    fix: 🐞 (root/hero) タイポミスを修正

--- packages/client/src/module/root/ui/hero.page.tsx
+++ packages/client/src/module/root/ui/hero.page.tsx
@@ -10,7 +10,7 @@ export const Hero: FC<HeroProps> = ({ className, ...props }) => (
     <div className="flex max-w-lg flex-col items-center space-y-10">
       <Avatar size={120} className="drop-shadow-xl" />
       <h1 className="font-rakkas text-5xl underline">Shio</h1>
-      <p className="font-rakkas text-lg font-semibold">フロント・バックエンドエンジニア。クリエイターのクリエイターになることを夢見てる。</p>
+      <p className="font-rakkas text-lg font-semibold">フロント・バックエンドエンジニア。クリエイターのためのクリエイターになることを夢見てる。</p>
     </div>
     <CodeBox className="hidden lg:block" />
   </div>
