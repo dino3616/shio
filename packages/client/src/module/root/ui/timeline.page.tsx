e1659f32f6d41706d1ba1f5aee945ed9fd36caf9
shio <85730998+dino3616@users.noreply.github.com>
2023-01-11 07:21:55 +0000

    fix: 🐞 (root/timeline) merginを修正

--- packages/client/src/module/root/ui/timeline.page.tsx
+++ packages/client/src/module/root/ui/timeline.page.tsx
@@ -17,7 +17,7 @@ export const Timeline: FC<TimelineProps> = ({ timelines, timelineItemProps, ...p
         aria-hidden
         className="absolute inset-0 m-[5px] w-0.5 bg-gray-600 before:absolute before:top-[-5px] before:left-[-5px] before:flex before:h-3 before:w-3 before:rounded-full before:border-4 before:border-solid before:border-gray-600 before:bg-gray-600 after:absolute after:bottom-[-5px] after:left-[-5px] after:flex after:h-3 after:w-3 after:rounded-full after:border-4 after:border-solid after:border-gray-600 after:bg-gray-600"
       />
-      <div className="space-y-5 py-5">
+      <div className="my-5 space-y-5">
         {timelines.map((timeline) => (
           <ScrollRevealAnimation key={timeline.id} duration={0.8} delay={0} distance="40px">
             <TimelineItem className="ml-7" timeline={timeline} {...timelineItemProps} />
