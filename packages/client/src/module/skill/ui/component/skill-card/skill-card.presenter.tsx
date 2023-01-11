f47f3e623440c28981a56457f14e4e8af6a86cdd
shio <85730998+dino3616@users.noreply.github.com>
2023-01-11 07:12:47 +0000

    feat: ✨ (skill/skill-card) skill-cardコンポーネントを作成

---
+++ packages/client/src/module/skill/ui/component/skill-card/skill-card.presenter.tsx
@@ -0,0 +1,26 @@
+import type { ComponentPropsWithoutRef, FC } from 'react';
+import { Card } from '@/common/component/card/card.presenter';
+import { Image } from '@/common/component/image/image.presenter';
+import { Link } from '@/common/component/link/link.presenter';
+import { twMerge } from '@/common/util/tw-merge.util';
+import type { Skill } from '@/module/skill/model/skill.model';
+
+export type SkillCardProps = Omit<ComponentPropsWithoutRef<typeof Card>, 'children'> & {
+  skill: Skill;
+};
+
+export const SkillCard: FC<SkillCardProps> = ({ skill, className, ...props }) => (
+  <Card className={twMerge('flex w-64 flex-col items-center overflow-hidden rounded-3xl p-0 drop-shadow-lg', className)} {...props}>
+    <Link href={skill.officialUrl} target="_blank" className="flex h-36 w-full items-center justify-center">
+      <Image src={skill.iconUrl} width={100} height={100} alt={skill.name} className="bg-white" />
+    </Link>
+    <div className="space-y-5 px-10 py-5">
+      <h2 className="text-center text-3xl font-semibold hover:underline">
+        <Link href={skill.officialUrl} target="_blank">
+          {skill.name}
+        </Link>
+      </h2>
+      <p>{skill.description}</p>
+    </div>
+  </Card>
+);
---
+++ packages/client/src/module/skill/ui/component/skill-card/skill-card.story.tsx
@@ -0,0 +1,22 @@
+import type { ComponentStoryObj, ComponentMeta } from '@storybook/react';
+import { SkillCard } from './skill-card.presenter';
+
+type Story = ComponentStoryObj<typeof SkillCard>;
+
+const meta: ComponentMeta<typeof SkillCard> = {
+  component: SkillCard,
+};
+
+export default meta;
+
+export const Default: Story = {
+  args: {
+    skill: {
+      id: '1',
+      name: 'StoryBook',
+      description: 'UI component dev & test: React, Vue, Angular',
+      officialUrl: 'https://storybook.js.org/',
+      iconUrl: 'https://raw.githubusercontent.com/gilbarbara/logos/1f372be75689d73cae89b6de808149b606b879e1/logos/storybook-icon.svg',
+    },
+  },
+};
