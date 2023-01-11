11678668c19e912cabf9c276afd96ce4c7155982
shio <85730998+dino3616@users.noreply.github.com>
2023-01-11 00:21:57 +0000

    feat: ✨ (module) skillをmodulesに追加

--- packages/api/src/module/index.ts
+++ packages/api/src/module/index.ts
@@ -1,4 +1,5 @@
+import { SkillModule } from './skill/skill.module';
 import { TimelineModule } from './timeline/timeline.module';
 import { UserModule } from './user/user.module';
 
-export const Modules = [UserModule, TimelineModule];
+export const Modules = [UserModule, TimelineModule, SkillModule];
