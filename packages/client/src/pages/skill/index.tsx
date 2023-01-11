bc2f184450bf816c5113a09e0616674fc6870ffe
shio <85730998+dino3616@users.noreply.github.com>
2023-01-11 07:22:21 +0000

    feat: ✨ (pages/skill) skillページを作成

---
+++ packages/client/src/pages/skill/index.tsx
@@ -0,0 +1,28 @@
+import type { GetStaticProps, NextPage } from 'next';
+import { Layout } from '@/module/layout/ui/layout.page';
+import type { Skill as SkillModel } from '@/module/skill/model/skill.model';
+import { findSkills } from '@/module/skill/repository/find-skills.repository';
+import { Skill } from '@/module/skill/ui/skill.page';
+
+export type SkillPageProps = {
+  skills: SkillModel[];
+};
+
+const SkillPage: NextPage<SkillPageProps> = ({ skills }: SkillPageProps) => (
+  <Layout title="Skill | shio.dev" className="py-10 px-5 md:px-10">
+    <Skill skills={skills} />
+  </Layout>
+);
+
+export const getStaticProps: GetStaticProps<SkillPageProps> = async () => {
+  const skills = await findSkills({});
+
+  return {
+    props: {
+      skills: JSON.parse(JSON.stringify(skills)),
+    },
+    revalidate: 20,
+  };
+};
+
+export default SkillPage;
