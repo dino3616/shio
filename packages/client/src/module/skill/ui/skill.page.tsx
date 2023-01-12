8108ca880333ed39d049be3302a16ade7ebd7d1c
shio <85730998+dino3616@users.noreply.github.com>
2023-01-12 08:23:27 +0000

    feat: ✨ (skill) learning-skill-listをskillに追加

--- packages/client/src/module/skill/ui/skill.page.tsx
+++ packages/client/src/module/skill/ui/skill.page.tsx
@@ -1,5 +1,6 @@
 import type { ComponentPropsWithoutRef, FC } from 'react';
 import type { Skill as SkillModel } from '../model/skill.model';
+import { LearningLevelList } from './component/learning-level-list/learning-level-list.presenter';
 import { SkillCard } from './component/skill-card/skill-card.presenter';
 import { Heading } from '@/common/component/heading/heading.presenter';
 import { ScrollRevealAnimation } from '@/common/component/scroll-reveal-animation/scroll-reveal-animation.presenter';
@@ -10,9 +11,10 @@ export type SkillProps = Omit<ComponentPropsWithoutRef<'div'>, 'children'> & {
 };
 
 export const Skill: FC<SkillProps> = ({ skills, className, ...props }) => (
-  <div className={twMerge('space-y-6', className)} {...props}>
+  <div className={twMerge('space-y-5', className)} {...props}>
     <Heading text="Skill" alt="スキル" />
-    <div className="my-5 flex gap-5">
+    <LearningLevelList />
+    <div className="flex gap-5">
       {skills.map((skill) => (
         <ScrollRevealAnimation key={skill.id} duration={0.8} delay={0} distance="40px">
           <SkillCard skill={skill} />
