1958f054a480cde75c1588b086f386995f609706
shio <85730998+dino3616@users.noreply.github.com>
2023-01-12 08:22:46 +0000

    feat: ✨ (skill/skill-card) learningLevelによって背景色を変更するように修正

--- packages/client/src/module/skill/ui/component/skill-card/skill-card.presenter.tsx
+++ packages/client/src/module/skill/ui/component/skill-card/skill-card.presenter.tsx
@@ -3,6 +3,7 @@ import { Card } from '@/common/component/card/card.presenter';
 import { Image } from '@/common/component/image/image.presenter';
 import { Link } from '@/common/component/link/link.presenter';
 import { twMerge } from '@/common/util/tw-merge.util';
+import { getLearningLevelBgColor } from '@/module/skill/model/learning-level.model';
 import type { Skill } from '@/module/skill/model/skill.model';
 
 export type SkillCardProps = Omit<ComponentPropsWithoutRef<typeof Card>, 'children'> & {
@@ -11,10 +12,15 @@ export type SkillCardProps = Omit<ComponentPropsWithoutRef<typeof Card>, 'childr
 
 export const SkillCard: FC<SkillCardProps> = ({ skill, className, ...props }) => (
   <Card className={twMerge('flex w-64 flex-col items-center overflow-hidden rounded-3xl p-0 drop-shadow-lg', className)} {...props}>
-    <Link href={skill.officialUrl} target="_blank" className="flex h-36 w-full items-center justify-center">
+    <Link
+      href={skill.officialUrl}
+      target="_blank"
+      className="flex h-36 w-full items-center justify-center"
+      style={{ ...getLearningLevelBgColor(skill.learningLevel) }}
+    >
       <Image src={skill.iconUrl} width={100} height={100} alt={skill.name} className="bg-white" />
     </Link>
-    <div className="space-y-5 px-10 py-5">
+    <div className="space-y-5 p-5">
       <h2 className="text-center text-3xl font-semibold hover:underline">
         <Link href={skill.officialUrl} target="_blank">
           {skill.name}
--- packages/client/src/module/skill/ui/component/skill-card/skill-card.story.tsx
+++ packages/client/src/module/skill/ui/component/skill-card/skill-card.story.tsx
@@ -1,5 +1,6 @@
 import type { ComponentStoryObj, ComponentMeta } from '@storybook/react';
 import { SkillCard } from './skill-card.presenter';
+import { LearningLevel } from '@/module/skill/model/learning-level.model';
 
 type Story = ComponentStoryObj<typeof SkillCard>;
 
@@ -15,6 +16,7 @@ export const Default: Story = {
       id: '1',
       name: 'StoryBook',
       description: 'UI component dev & test: React, Vue, Angular',
+      learningLevel: LearningLevel.Beginner,
       officialUrl: 'https://storybook.js.org/',
       iconUrl: 'https://raw.githubusercontent.com/gilbarbara/logos/1f372be75689d73cae89b6de808149b606b879e1/logos/storybook-icon.svg',
     },
