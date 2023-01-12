ceb9289f808a351b40a2e21649cad6d660679050
shio <85730998+dino3616@users.noreply.github.com>
2023-01-12 08:18:41 +0000

    feat: ✨ (skill/model) learningLevelをプロパティに追加

--- packages/client/src/module/skill/model/skill.model.ts
+++ packages/client/src/module/skill/model/skill.model.ts
@@ -1,7 +1,10 @@
+import type { LearningLevel } from './learning-level.model';
+
 export type Skill = {
   id: string;
   name: string;
   description: string;
+  learningLevel: LearningLevel;
   officialUrl: string;
   iconUrl: string;
 };
