25137732d276cbc92ab798cba9b7fe047e9e3d7d
shio <noir3616@gmail.com>
2023-01-13 08:45:15 +0000

    feat: 🐞 (about) sns-listをoutlineに統合

--- packages/client/src/pages/about/index.tsx
+++ packages/client/src/pages/about/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next';
 import { Outline } from '@/module/about/ui/outline.page';
-import { SnsList } from '@/module/about/ui/sns-list.page';
 import { Layout } from '@/module/layout/ui/layout.page';
 
 const AboutPage: NextPage = () => (
   <Layout title="About | shio.dev" className="space-y-5 py-10 px-5 md:px-10">
-    <Outline className="border-b-2 border-gray-400 pb-10" />
-    <SnsList className="flex justify-end" />
+    <Outline />
   </Layout>
 );
 
