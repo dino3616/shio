559119cdb22b68605c9a5a57a3258d37357bd9d0
shio <noir3616@gmail.com>
2023-01-15 10:07:39 +0000

    asset: 🍱 (ogp) 拡張子をsvgからpngに修正

--- packages/client/src/module/layout/ui/component/meta/meta.presenter.tsx
+++ packages/client/src/module/layout/ui/component/meta/meta.presenter.tsx
@@ -17,8 +17,8 @@ export const Meta: FC<MetaProps> = ({ title, hostname }) => (
     <meta property="description" content="shioのポートフォリオサイト" key={MetaKey.DESCRIPTION} />
     <meta property="og:description" content="shioのポートフォリオサイト" key={MetaKey.OGDESCRIPTION} />
     <meta property="og:image" content="/ogp/ogp.png" key={MetaKey.IMAGE} />
-    <meta property="og:image" content={`https://${hostname}/ogp.svg`} key={MetaKey.IMAGE} />
-    <meta property="twitter:image" content={`https://${hostname}/ogp.svg`} key={MetaKey.TWITTER_IMAGE} />
+    <meta property="og:image" content={`https://${hostname}/ogp.png`} key={MetaKey.IMAGE} />
+    <meta property="twitter:image" content={`https://${hostname}/ogp.png`} key={MetaKey.TWITTER_IMAGE} />
     <meta name="twitter:card" content="summary_large_image" key={MetaKey.TWITTER_CARD} />
     <meta name="twitter:site" content="shio" key={MetaKey.TWITTER_SITE} />
     <meta name="twitter:creator" content="shio" key={MetaKey.TWITTER_CREATOR} />
