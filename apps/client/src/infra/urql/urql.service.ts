577930c272926867dab45ca47ff49f479c4abb07
shio <85730998+dino3616@users.noreply.github.com>
2023-02-07 14:25:39 +0000

    fix: 🐞 (infra/urql/service) インデックスシグネチャに基づいた記法に修正

--- apps/client/src/infra/urql/urql.service.ts
+++ apps/client/src/infra/urql/urql.service.ts
@@ -3,7 +3,7 @@ import { createClient as createWsClient } from 'graphql-ws';
 import { createClient, ClientOptions, cacheExchange, dedupExchange, fetchExchange, subscriptionExchange, Exchange } from 'urql';
 import { scalarExchange } from './exchange/scalar.exchange';
 
-const url = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_DEVELOP || process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT;
+const url = process.env['NEXT_PUBLIC_GRAPHQL_ENDPOINT_DEVELOP'] || process.env['NEXT_PUBLIC_GRAPHQL_ENDPOINT'];
 if (!url) {
   throw new Error('"NEXT_PUBLIC_GRAPHQL_ENDPOINT" または "NEXT_PUBLIC_GRAPHQL_ENDPOINT_DEVELOP"は必須の環境変数です.');
 }
@@ -11,7 +11,7 @@ if (!url) {
 const exchanges: Exchange[] = [devtoolsExchange, dedupExchange, scalarExchange, cacheExchange, fetchExchange];
 
 if (typeof window !== 'undefined') {
-  const wsUrl = process.env.NEXT_PUBLIC_WS_ENDPOINT_DEVELOP || process.env.NEXT_PUBLIC_WS_ENDPOINT;
+  const wsUrl = process.env['NEXT_PUBLIC_WS_ENDPOINT_DEVELOP'] || process.env['NEXT_PUBLIC_WS_ENDPOINT'];
   if (!wsUrl) {
     throw new Error('"NEXT_PUBLIC_WS_ENDPOINT" または "NEXT_PUBLIC_WS_ENDPOINT_DEVELOP"は必須の環境変数です.');
   }
