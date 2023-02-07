5d005a81b2a2a489092dcdc62d9479b8e4acd4d0
shio <85730998+dino3616@users.noreply.github.com>
2023-02-07 13:28:45 +0000

    feat: ⚰️ (user) 未使用のuser moduleを削除

--- apps/api/src/module/index.ts
+++ apps/api/src/module/index.ts
@@ -1,5 +1,4 @@
 import { SkillModule } from './skill/skill.module';
 import { TimelineModule } from './timeline/timeline.module';
-import { UserModule } from './user/user.module';
 
-export const Modules = [UserModule, TimelineModule, SkillModule];
+export const Modules = [SkillModule, TimelineModule];
