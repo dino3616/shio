e03d768d20b14268ae2b031c2f1d1a70e0254cf3
shio <noir3616@gmail.com>
2023-02-09 02:42:17 +0000

    chore: 🔧 (stylelint) 各scriptの設定を追加

--- packages/stylelint/index.js
+++ packages/stylelint/index.js
@@ -1,10 +1,5 @@
 /** @type {import("stylelint").Config} */
 module.exports = {
-  plugins: ["stylelint-prettier"],
-  extends: [
-    "stylelint-config-standard-scss",
-    "stylelint-config-recess-order",
-    "stylelint-prettier/recommended",
-    "stylelint-config-prettier",
-  ],
+  plugins: ['stylelint-prettier'],
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order', 'stylelint-prettier/recommended', 'stylelint-config-prettier'],
 };
