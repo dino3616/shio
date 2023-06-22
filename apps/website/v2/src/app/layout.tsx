4c4eb9efe357fce78255d1989ee59f44d1312238
shio <85730998+dino3616@users.noreply.github.com>
2023-06-22 10:21:33 +0900

    feat: ✨ (layout) RootLayout を作成

---
+++ apps/website/v2/src/app/layout.tsx
@@ -0,0 +1,64 @@
+import { Analytics } from '@vercel/analytics/react';
+import type { Metadata } from 'next';
+import type { ReactNode } from 'react';
+import { fontFamily } from '@/core/font/family';
+import { cn } from '@/core/util/cn';
+import { getBaseUrl } from '@/core/util/get-base-url';
+import { Footer } from '@/module/root/ui/layout/footer';
+import { Header } from '@/module/root/ui/layout/header';
+import { PageTransitionAnimationProvider } from '@/module/root/ui/layout/page-transition-animation-provider';
+import { ThemeProvider } from '@/module/root/ui/layout/theme-provider';
+import { colors } from '@/style/token';
+import '@/style/global.css';
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+const RootLayout = ({ children }: RootLayoutProps): ReactNode => (
+  <html lang="en" suppressHydrationWarning>
+    <head />
+    <body
+      className={cn(
+        fontFamily,
+        'flex min-h-screen flex-col bg-purple-1 font-sans',
+        'scrollbar-thin scrollbar-track-transparent scrollbar-thumb-mauve-6 scrollbar-thumb-rounded-full hover:scrollbar-thumb-mauve-7',
+        'bg-grid-light-purple-5/50 dark:bg-grid-dark-purple-5/50',
+      )}
+    >
+      <Analytics />
+      <ThemeProvider attribute="data-theme">
+        <Header className="fixed left-0 top-0 z-10 grow-0" />
+        <PageTransitionAnimationProvider>
+          <main className="min-h-full grow">{children}</main>
+        </PageTransitionAnimationProvider>
+        <Footer />
+      </ThemeProvider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
+
+export const metadata: Metadata = {
+  metadataBase: getBaseUrl(),
+  title: {
+    default: 'shio.studio | Provide accessible Web for everyone, everywhere.',
+    template: '%s | shio.studio',
+  },
+  description:
+    'This portfolio presents my projects and thoughts as a frontend developer who maximizes website accessibility from two perspectives: engineer and designer.',
+  openGraph: {
+    locale: 'en_US',
+    url: getBaseUrl(),
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@shio3616',
+    creator: '@shio3616',
+  },
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: colors.light.purple['7'] },
+    { media: '(prefers-color-scheme: dark)', color: colors.dark.purple['7'] },
+  ],
+};
