b67544224e3b9d3a436738a18f3f15f1d17d4b0c
shio <85730998+dino3616@users.noreply.github.com>
2023-08-10 11:33:33 +0900

    fix: 🐛 (core/image) placeholder を外部から注入できない問題を修正

--- packages/core/component/image/image.presenter.tsx
+++ packages/core/component/image/image.presenter.tsx
@@ -4,7 +4,7 @@ import { forwardRef } from 'react';
 
 type ImageProps = ComponentPropsWithRef<typeof NextImage>;
 
-export const Image = forwardRef<ElementRef<typeof NextImage>, Omit<ImageProps, 'ref'>>(({ placeholder, priority, ...props }, ref) => (
+export const Image = forwardRef<ElementRef<typeof NextImage>, Omit<ImageProps, 'ref'>>(({ priority, ...props }, ref) => (
   <NextImage ref={ref} placeholder={priority ? undefined : 'blur'} {...props} />
 ));
 
