2ae42eb9c13c0302adeaba1ad1a92d4ce5d53b61
shio <85730998+dino3616@users.noreply.github.com>
2023-08-30 14:52:33 +0900

    chore: 🔧 (eslint) 不要な設定の上書きを削除

--- packages/design-token/.eslintrc.cjs
+++ packages/design-token/.eslintrc.cjs
@@ -1,12 +1,4 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['shio-esm'],
-  rules: {
-    'no-restricted-imports': [
-      'error',
-      {
-        patterns: ['../'],
-      },
-    ],
-  },
 };
