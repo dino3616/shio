6ab0ba1ded647148d0c1e2adebf9944e97c4ea41
shio <85730998+dino3616@users.noreply.github.com>
2023-09-29 10:24:04 +0900

    chore: 🔧 (website-v2) gql-codegen の設定を作成

---
+++ apps/website-v2/codegen.ts
@@ -0,0 +1,20 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: './graphql.schema.json',
+  documents: ['src/infra/graphql/document/**/*.gql'],
+  generates: {
+    './src/infra/graphql/generated/graphql.ts': {
+      config: {
+        scalars: {
+          DateTime: Date,
+        },
+        withHooks: false,
+      },
+      plugins: ['typescript', 'typescript-operations', 'typescript-urql'],
+    },
+  },
+};
+
+export default config;
