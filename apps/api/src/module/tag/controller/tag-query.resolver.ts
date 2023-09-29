3db8a0b9914b7aaea8d3b3ddc1ee59dd898af4f0
shio <85730998+dino3616@users.noreply.github.com>
2023-09-29 10:22:03 +0900

    feat: ✨ (api/tag) tag-query resolver を作成

---
+++ apps/api/src/module/tag/controller/tag-query.resolver.ts
@@ -0,0 +1,31 @@
+import { Inject, Logger } from '@nestjs/common';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { InjectionToken } from '@/common/constant/injection-token';
+import type { Tag } from '@/module/tag/domain/tag.model';
+// TODO: Once this issue is resolved, modify to use `import type` syntax.
+// https://github.com/typescript-eslint/typescript-eslint/issues/5468
+import { type TagUseCaseInterface } from '@/module/tag/use-case/tag.use-case';
+import { TagWhereUniqueInput } from './dto/input/tag-where-unique.input';
+import { TagObject } from './dto/object/tag.object';
+
+@Resolver()
+export class TagQuery {
+  private readonly logger = new Logger(TagQuery.name);
+
+  constructor(
+    @Inject(InjectionToken.TAG_USE_CASE)
+    private readonly tagUseCase: TagUseCaseInterface,
+  ) {}
+
+  @Query(() => TagObject, { nullable: true })
+  async findTag(
+    @Args('where', { type: () => TagWhereUniqueInput })
+    where: TagWhereUniqueInput,
+  ): Promise<Tag | null> {
+    this.logger.log(`${this.findTag.name} called`);
+
+    const foundTag = await this.tagUseCase.findTag(where.tagId);
+
+    return foundTag;
+  }
+}
