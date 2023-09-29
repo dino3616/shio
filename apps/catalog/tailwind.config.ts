b9ebfcfe9899aa8b0f2e99f605d1ca65ddb5ee12
shio <85730998+dino3616@users.noreply.github.com>
2023-09-29 10:23:20 +0900

    fix: 🐛 (catalog) 型定義ファイルの読み込みでループが発生するバグを修正

--- apps/catalog/tailwind.config.ts
+++ apps/catalog/tailwind.config.ts
@@ -2,7 +2,7 @@ import { createConfig } from '@shio/tailwind/config';
 
 const config = createConfig((defaultConfig) => ({
   ...defaultConfig,
-  content: ['./.storybook/**/*.{.ts,.tsx}', '../../**/*.{ts,tsx}'],
+  content: ['./.storybook/**/*.{ts,tsx}', '../../**/*.{ts,tsx}', '!../../**/*.d.ts'],
 }));
 
 export default config;
