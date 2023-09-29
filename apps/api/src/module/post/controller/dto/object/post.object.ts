5cacfb5462dd56e6d010939eb3aa454a21d0a048
shio <85730998+dino3616@users.noreply.github.com>
2023-09-29 10:16:48 +0900

    feat: ✨ (api/post) post object を作成

---
+++ apps/api/src/module/post/controller/dto/object/post.object.ts
@@ -0,0 +1,33 @@
+import { Field, ID, Int, ObjectType } from '@nestjs/graphql';
+import { PostCategoryEnum } from '@/module/post/controller/dto/enum/post-category.enum';
+import type { Post } from '@/module/post/domain/post.model';
+
+@ObjectType('Post')
+export class PostObject implements Omit<Post, 'tagIds' | 'isPinned'> {
+  @Field(() => ID, { nullable: false })
+  id!: string;
+
+  @Field(() => Int, { nullable: true })
+  pinIndex!: number | null;
+
+  @Field(() => String, { nullable: false })
+  title!: string;
+
+  @Field(() => String, { nullable: false })
+  thumbnailUrl!: string;
+
+  @Field(() => String, { nullable: false })
+  content!: string;
+
+  @Field(() => PostCategoryEnum, { nullable: false })
+  category!: PostCategoryEnum;
+
+  @Field(() => Int, { nullable: false })
+  views!: number;
+
+  @Field(() => Date, { nullable: false })
+  createdAt!: Date;
+
+  @Field(() => Date, { nullable: false })
+  updatedAt!: Date;
+}
