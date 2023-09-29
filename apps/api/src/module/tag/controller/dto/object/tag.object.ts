39c52f613356ae3eec302cec2ad8af45be4d36c5
shio <85730998+dino3616@users.noreply.github.com>
2023-09-29 10:21:04 +0900

    feat: ✨ (api/tag) tag object を作成

---
+++ apps/api/src/module/tag/controller/dto/object/tag.object.ts
@@ -0,0 +1,14 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import type { Tag } from '@/module/tag/domain/tag.model';
+
+@ObjectType('Tag')
+export class TagObject implements Omit<Tag, 'postIds'> {
+  @Field(() => ID, { nullable: false })
+  id!: string;
+
+  @Field(() => String, { nullable: false })
+  uniqueName!: string;
+
+  @Field(() => String, { nullable: false })
+  displayName!: string;
+}
